bffdc8b46dc88dfc1592bef12b31a73dc3885a9a
Mathias <mathiasbergqvist88@gmail.com>
2023-08-14 19:53:48 +0200

    Setup api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const api = require("./routes/api");
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(
 );
 app.use(morgan("combined"));
 app.use(express.json());
+app.use("/v1", api);
 
 module.exports = app;
---
+++ src/routes/api.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const teamsRouter = require("./teams/teams.router");
+
+const api = express.Router();
+
+api.use("/teams", teamsRouter);
+
+module.exports = api;
