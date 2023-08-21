1803ad708a1013e7453fa57f1d54e5f57b5b5d86
Mathias <mathiasbergqvist88@gmail.com>
2023-08-21 20:31:01 +0200

    Mongoose schema for player and tema

---
+++ src/models/player.mongo.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+    },
+    position: {
+        type: String,
+        enum: ["goalkeeper", "defender", "midfielder", "forward"],
+        required: true,
+    },
+    league: {
+        type: String,
+        required: true,
+    },
+    countryUnicode: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("Player", playerSchema);
---
+++ src/models/teams.mongo.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const teamsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    stadium: {
+        type: String,
+        required: true,
+    },
+    manager: {
+        type: String,
+        required: true,
+    },
+    league: {
+        type: String,
+        required: true,
+    },
+    players: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Player'
+    }]
+});
+
+module.exports = mongoose.model("Team", teamsSchema);
