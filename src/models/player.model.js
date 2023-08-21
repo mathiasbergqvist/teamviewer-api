d6145c2a449afc8afa366d48b4a53cd747311faf
Mathias <mathiasbergqvist88@gmail.com>
2023-08-21 22:32:53 +0200

    Player model

---
+++ src/models/player.model.js
@@ -0,0 +1,29 @@
+const Player = require("./player.mongo");
+
+const getAllPlayers = async () => await Player.find({});
+
+const getPlayerById = async (playerId) => await Player.findById(playerId);
+
+const savePlayer = async (newPlayer) => {
+    const {name, number, position, league, countryUnicode} = newPlayer;
+        const playerToSave = new Player({
+            name,
+            number,
+            position,
+            league,
+            countryUnicode
+        });
+    return await Player.create(playerToSave)
+};
+
+const updatePlayer = async (playerId, updatedPlayer) => await Player.findByIdAndUpdate(playerId, updatePlayer);
+
+const deletePlayer = async (playerId) => await Player.findOneAndDelete({_id: playerId});
+
+module.exports = {
+    getAllPlayers,
+    getPlayerById,
+    savePlayer,
+    updatePlayer,
+    deletePlayer,
+};
