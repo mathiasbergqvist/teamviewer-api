7e218e05e5439f5b60283783be197f230a8ac71c
Mathias <mathiasbergqvist88@gmail.com>
2023-08-21 22:31:56 +0200

    Setup connection to mongo db

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const app = require("./app");
+const {mongoConnect} = require("./services/mongo");
 require("dotenv").config();
+
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 const startServer = async () => {
+    
+    await mongoConnect();
+
     server.listen(PORT, () => {
         console.log(`🚀 Listening on port ${PORT}`);
     });
---
+++ src/services/mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MOGO_DB_URL = process.env.MOGO_DB_URL;
+
+mongoose.connection.once("open", () => {
+    console.log("🔌 MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (error) => {
+    console.error(`Error connecting to MongoDB: ${error}`);
+});
+
+const mongoConnect = async () => {
+    await mongoose.connect(MOGO_DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+const mongoDisconnect = async () => {
+    await mongoose.disconnect();
+};
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+};
