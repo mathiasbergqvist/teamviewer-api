07102d6fba8fbf83f97ac99ccabe9a615be8ad68
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2023-09-06 19:59:05 +0200

    Add basic auth

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const api = require("./routes/api");
+const basicAuth = require("express-basic-auth");
+require("dotenv").config();
+
+const username = process.env.BASIC_AUTH_USERNAME;
+const password = process.env.BASIC_AUTH_PASSWORD;
+const users = {};
+users[username] = password;
 
 const app = express();
-
 app.use(
     cors({
         origin: "http://localhost:4001",
@@ -13,6 +19,14 @@ app.use(
 );
 app.use(morgan("combined"));
 app.use(express.json());
+app.use(
+    basicAuth({
+        users,
+        unauthorizedResponse: {
+        message: "Unauthorized",
+    },
+    })
+);    
 app.use("/v1", api);
 
 module.exports = app;
