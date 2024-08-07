6a0704a73a508b4972fd2634f98223cad9560b06
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2024-08-07 14:44:50 +0200

    Add eslint support

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const pluginSecurity = require('eslint-plugin-security');
+const nodePlugin = require('eslint-plugin-n');
+const js = require('@eslint/js');
+
+module.exports = [
+    js.configs.recommended,
+    pluginSecurity.configs.recommended,
+    nodePlugin.configs['flat/recommended-script'],
+    {
+        files: ['**/*.js'],
+        plugins: {
+            security: pluginSecurity,
+        },
+        rules: {
+            'n/exports-style': ['error', 'module.exports'],
+        },
+    },
+    {
+        ignores: ['**/*.test.js'],
+    },
+];
